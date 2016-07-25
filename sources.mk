12ca9513aa036bf255beb3bd5d88b139054ae531
F145h <its.f145h@gmail.com>
2016-07-25 23:02:09 +0300

    fix makefiles

--- sources.mk
+++ sources.mk
@@ -1,7 +1,7 @@
 SRC_FILES= ./src/main.cpp \
            ./src/errorpage.cpp \
            ./src/applicationloader.cpp \
-		   ./src/requestmanager.cpp \
+           ./src/requestmanager.cpp \
            ./src/connection.cpp \
            ./src/connectionthread.cpp \
            ./src/mimefiletypes.cpp \
@@ -15,4 +15,4 @@ SRC_FILES= ./src/main.cpp \
            ./src/3rdparty/http_parser/http_parser.c \
            ./src/3rdparty/multipart_parser/multipart_parser.c \
            ./src/3rdparty/md5/md5.cpp \
-           ./src/3rdparty/pugixml/pugixml.cpp \
+           ./src/3rdparty/pugixml/pugixml.cpp
